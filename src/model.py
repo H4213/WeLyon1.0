bf64b33c24e2c84d00347f71606324fc04697a08
A. El Ouassouli <amine.elouassouli@gmail.com>
2015-05-04 09:14:05 +0200

    Dev new structure OP

--- src/model.py
+++ src/model.py
@@ -10,11 +10,7 @@ from sqlalchemy.orm import backref, relation
 import datetime
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tmucotknskzdvn:B5Hyna3G7I1xIhPj3i_CSdl-GS@ec2-54-163-238-96.compute-1.amazonaws.com:5432/d6fisokcj01ulm'
-<<<<<<< HEAD
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://H4213:SabreESS32@82.241.33.248:3306/WeLyon-paul'
-=======
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://H4213:SabreESS32@82.241.33.248:3306/WeLyon-simple'
->>>>>>> origin/dev-MergeNewStructure
 db = SQLAlchemy(app)
  
 ########################################################################
